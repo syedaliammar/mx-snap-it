91f9b8660205948abbe678acdfd884fdc5d41a75
Ammar Rizvi <ammarizvi@gmail.com>
2021-01-24 16:44:41 +0100

    First working version

--- snapIt/src/SnapIt.jsx
+++ snapIt/src/SnapIt.jsx
@@ -1,12 +1,34 @@
 import { Component, createElement } from "react";
 import { hot } from "react-hot-loader/root";
+import * as htmlToImage from "html-to-image";
+import { toPng, toJpeg, toSvg } from "html-to-image";
+import { saveAs } from "file-saver";
 
-import "./ui/SnapIt.css";
+const SnapIt = props => {
+    const { classNameToExport, exportFileName } = props;
 
-class SnapIt extends Component {
-    render() {
-        return <div />;
-    }
-}
+    const _exportToSVG = elementToExport => {
+        htmlToImage
+            .toSvg(elementToExport)
+            .then(function(dataUrl) {
+                saveAs(dataUrl, exportFileName + ".svg");
+            })
+            .catch(function(error) {
+                console.error("Something went wrong while generating the SVG", error);
+            });
+    };
+
+    return (
+        <div id="modebar">
+            <button
+                id="export_org_svg"
+                title="Download chart as SVG"
+                onClick={() => _exportToSVG(document.getElementsByClassName("mx-name-" + classNameToExport)[0])}
+            >
+                SVG
+            </button>
+        </div>
+    );
+};
 
 export default hot(SnapIt);
